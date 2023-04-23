7a1cc443029dcc10a0f18ae5128ec9f45c57d328
vrglhuhu <cheanbvergel@gmail.com>
2023-04-23 21:37:24 +0800

    The Psuedocode.

--- _FileReader_HighestGWA_Presenter.py
+++ _FileReader_HighestGWA_Presenter.py
@@ -1,3 +1,15 @@
 # Vergel, Chean Bernard Villanueva
 # Assignment No. 4
-# Question No. 1
+# Question No. 2
+
+# Create Header
+# Open the file
+# Initialize the highest GWA
+# Iterate over the lines
+  # Convert the GWA to float
+  # Compare the GWA with the highest GWA
+    # Update the highest GWA
+    # Update the name
+# Print the result
+# Create Footer
+
