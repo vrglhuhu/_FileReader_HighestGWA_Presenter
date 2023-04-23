0ec247d9c45d19a8fa0886cd9f7721a6bf80d4d1
vrglhuhu <cheanbvergel@gmail.com>
2023-04-23 21:40:50 +0800

    Creating Header.

--- _FileReader_HighestGWA_Presenter.py
+++ _FileReader_HighestGWA_Presenter.py
@@ -3,6 +3,17 @@
 # Question No. 2
 
 # Create Header
+import pyfiglet
+
+print("")
+print("=" * 80)
+print("")
+welcome = pyfiglet.figlet_format("Welcome to my space".center(57, " "), font="digital")
+print(welcome)
+print("")
+print("=" * 80)
+print("\033[33mHi, I am Chean Bernard V. Vergel a first year college student at Polytechnic University of the Philippines.\033[0m")
+print("")
 # Open the file
 # Initialize the highest GWA
 # Iterate over the lines
