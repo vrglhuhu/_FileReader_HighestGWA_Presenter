11a8bec199070be6d1acae8745c61c31e6d0c3d5
vrglhuhu <cheanbvergel@gmail.com>
2023-04-23 22:59:36 +0800

    Adding spaces and stating the purpose the code.

--- _FileReader_HighestGWA_Presenter.py
+++ _FileReader_HighestGWA_Presenter.py
@@ -14,32 +14,48 @@ print("")
 print("=" * 80)
 print("\033[33mHi, I am Chean Bernard V. Vergel a first year college student at Polytechnic University of the Philippines.\033[0m")
 print("")
+
 # Ask for the name of the user
 name_user = input("\033[40mHow about you what is your name? \033[0m")
 print("")
 print(f"\033[40m\033[33mHi, {name_user}! I welcome you on this program. \033[0m")
 print("")
+
+# Stating the purpose of this program
+print("\033[40m\033[35mThis program will output the highest GWA together with the name.\033[0m")
+print("")
+
 # Open the file
 with open("name_gwa_data.txt", "r") as file_handle:
+  
   # Read the file line by line
   lines = file_handle.readlines()
+
 # Initialize the highest GWA
 highest_gwa = 5
+
 # Iterate over the lines
 for line in lines:
+  
   # Split the line into name and GWA
   name, gwa = line.split()
+
   # Convert the GWA to float
   gwa = float(gwa)
+
   # Compare the GWA with the highest GWA
   if gwa < highest_gwa :
+
     # Update the highest GWA
     highest_gwa = gwa
+
     # Update the name
     highest_name = name
+
 # Print the result
 print(f"\033[40m\033[34mThe student with the highest GWA is", highest_name, "with a GWA of", highest_gwa, ".\033[0m")
 print("")
+
 # Create Footer
 print(f"\033[40m\033[33mThank you for your time, {name_user}! \033[0m")
 print("")
